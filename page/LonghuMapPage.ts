2d887bb58e07f5d0c552437aa7dd35d38a5ecc1a
xiaoer <xiaoer@example.com>
2019-09-12 15:02:30 +0800

    client批量提交

--- page/LonghuMapPage.ts
+++ page/LonghuMapPage.ts
@@ -1036,14 +1036,14 @@ module gamelonghu.page {
                 TongyongPageDef.ins.alertRecharge(StringU.substitute("老板，您的金币少于{0}哦~\n补充点金币去大杀四方吧~", PLAYER_LEAST_MONEY), () => {
                     this._game.uiRoot.general.open(DatingPageDef.PAGE_CHONGZHI);
                 }, () => {
-                }, false, TongyongPageDef.TIPS_SKIN_STR['cz']);
+                }, true, TongyongPageDef.TIPS_SKIN_STR['cz']);
                 return;
             }
             if (!this._curChip || this._curChip > money) {
                 TongyongPageDef.ins.alertRecharge("老板，您的金币不足哦~\n补充点金币去大杀四方吧~", () => {
                     this._game.uiRoot.general.open(DatingPageDef.PAGE_CHONGZHI);
                 }, () => {
-                }, false, TongyongPageDef.TIPS_SKIN_STR['cz']);
+                }, true, TongyongPageDef.TIPS_SKIN_STR['cz']);
                 return;
             }
             this._betWait = true;
